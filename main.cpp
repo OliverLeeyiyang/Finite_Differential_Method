13cea646dbd842792885abc7c49d7da8dc25ea4f
Yiyang <ge89yes@tum.de>
2024-02-11 11:18:43 +0100

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,5 +6,17 @@
 
 int main()
 {
+    FDM::Vector<double> vec1(3, 1.0);
+    FDM::Vector<double> vec2(3);
+    std::vector<double> vec = {1.0, 2.0, 3.0};
+    FDM::Vector<double> vec3(vec);
+    FDM::Vector<double> vec4(vec1);
+
+    // Print
+    vec1.print();
+    vec2.print();
+    vec3.print();
+    vec4.print();
+    
     return 0;
 }
